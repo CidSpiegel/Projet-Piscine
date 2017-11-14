59ce0155b58e4cd9cc021b91767741a9e176d12c
Pierre Michel <pierre.michel014@gmail.com>
2017-11-14 22:36:08 +0100

    Mise à jour scripts

---
+++ Assets/Scripts/Perso Manager/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+	public float Vitesse;
+
+	void Update()
+	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * Vitesse;
+		var y = Input.GetAxis("Vertical") * Time.deltaTime * Vitesse;
+
+		transform.Translate(x, y, 0);
+	}
+}
